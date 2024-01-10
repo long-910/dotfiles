4689cc1051c7e61312164677d3fadb0d53d2156a
long-910 <910.long@gmail.com>
2024-01-11 02:10:12 +0900

    update install_and_configure.sh

--- install_and_configure.sh
+++ install_and_configure.sh
@@ -6,16 +6,16 @@ base_repo_url="https://raw.githubusercontent.com/long-910/dotfiles/main"
 # 関数: パッケージのインストール状態を確認して表示する
 check_and_install() {
     package_name=$1
-    show_status "Checking $package_name"
+    show_status "⚙️  Checking $package_name"
     if command -v $package_name &> /dev/null; then
         installed_version=$($package_name --version)
-        echo "$package_name は既にインストールされています (バージョン: $installed_version)"
+        echo "✅ $package_name is already installed (Version: $installed_version)"
     else
-        show_status "Installing $package_name"
+        show_status "🔧 Installing $package_name"
         sudo apt update
         sudo apt install -y $package_name
         installed_version=$($package_name --version)
-        echo "$package_name をインストールしました (バージョン: $installed_version)"
+        echo "✅ $package_name installed successfully (Version: $installed_version)"
     fi
 }
 
@@ -25,47 +25,40 @@ get_and_apply_config() {
     config_file=$2
     show_status "Configuring $package_name"
     
-    if [ -e "$config_file" ]; then
+    if [ -e "${HOME}/${config_file}" ]; then
         # 既存の設定ファイルが存在する場合はバックアップを作成
-        backup_file="${config_file}_backup_$(date +"%Y%m%d%H%M%S")"
-        mv "$config_file" "$backup_file"
-        echo "既存の $package_name の設定ファイルをバックアップしました: $backup_file"
+        backup_file="${HOME}/${config_file}_backup_$(date +"%Y%m%d%H%M%S")"
+        mv "${HOME}/${config_file}" "$backup_file"
+        echo "📁 Backup created for existing $package_name config file: $backup_file"
     fi
     
     # リポジトリ内のdotfilesディレクトリから設定ファイルを取得
-    curl -fsSL "$base_repo_url/dotfiles/$config_file" -o "$config_file"
-    echo "$package_name の設定ファイルを取得しました: $config_file"
+    curl -fsSL "$base_repo_url/dotfiles/$config_file" -o "${HOME}/${config_file}"
+    echo "✅ $package_name configuration applied successfully: ${HOME}/${config_file}"
 }
 
 # 関数: ステータスメッセージを表示する
 show_status() {
-    echo "----- $1 の確認・インストール中 -----"
+    echo -e "\033[1;34m$1\033[0m"  # 青色の文字で表示
 }
 
 # スクリプトの進捗メッセージを表示
-echo "----- スクリプトの進行中 -----"
+echo "🚀 Script is in progress..."
 
 # Zshのインストールと設定ファイルの取得・適用
 check_and_install "zsh"
-get_and_apply_config "Zsh" "$HOME/.zshrc"
+get_and_apply_config "Zsh" ".zshrc"
 
 # Tmuxのインストールと設定ファイルの取得・適用
 check_and_install "tmux"
-get_and_apply_config "Tmux" "$HOME/.tmux.conf"
+get_and_apply_config "Tmux" ".tmux.conf"
 
 # tmux-mem-cpu-loadのインストール
 check_and_install "tmux-mem-cpu-load"
 
 # Emacsのインストールと設定ファイルの取得・適用
 check_and_install "emacs"
-get_and_apply_config "Emacs" "$HOME/.emacs"
-
-# Oh My Zshのインストールと設定ファイルの取得・適用
-check_and_install "sh"
-get_and_apply_config "Oh My Zsh" "$HOME/.oh-my-zsh"
-
-# ターミナルを再起動してZshをデフォルトに設定
-exec zsh
+get_and_apply_config "Emacs" ".emacs"
 
 # スクリプトの進捗メッセージを終了
-echo "----- スクリプトの実行が完了しました -----"
+echo "🎉 Script execution completed!"
